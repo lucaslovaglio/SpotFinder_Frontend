6cae7ac779dea3116e00f3a940aa98e43b5b652f
lucaslovaglio <lucas.lovaglio@ing.austral.edu.ar>
2023-07-15 12:08:46 -0300

    QrScanner version sin react

--- src/components/QrScanner.jsx
+++ src/components/QrScanner.jsx
@@ -1,6 +1,7 @@
 // file = Html5QrcodePlugin.jsx
 import { Html5QrcodeScanner } from 'html5-qrcode';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import "../styles/qrScanner.css"
 
 const qrcodeRegionId = "html5qr-code-full-region";
 
@@ -22,9 +23,32 @@ const createConfig = (props) => {
     return config;
 };
 
-const Html5QrcodePlugin = (props) => {
+const QrScanner = (props) => {
 
-    useEffect(() => {
+    const verifyPermissions = async () => {
+        try {
+            const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+            stream.getTracks().forEach((track) => {
+              track.stop(); // Detiene el acceso a la cámara
+            });
+            console.log(11111111)
+            console.log(stream)
+            if (stream.active === false) {
+                setShowButton(true)
+            }
+            else {
+                setShowButton(false); // Oculta el botón
+            }
+            // Aquí puedes agregar la lógica adicional que deseas ejecutar
+          } catch (error) {
+            console.error('Error al acceder a la cámara:', error);
+            setShowButton(true)
+            console.log(2222222)
+            // Manejo del error (opcional)
+          }
+    }
+
+    useEffect(async () => {
         // when component mounts
         const config = createConfig(props);
         const verbose = props.verbose === true;
@@ -35,17 +59,45 @@ const Html5QrcodePlugin = (props) => {
         const html5QrcodeScanner = new Html5QrcodeScanner(qrcodeRegionId, config, verbose);
         html5QrcodeScanner.render(props.qrCodeSuccessCallback, props.qrCodeErrorCallback);
 
+        // await verifyPermissions()
+        console.log(showButton)
+
         // cleanup function when component will unmount
         return () => {
             html5QrcodeScanner.clear().catch(error => {
                 console.error("Failed to clear html5QrcodeScanner. ", error);
             });
         };
+        
     }, []);
 
+
+    const [showButton, setShowButton] = useState(true);
+    const handleClick = async () => {
+        const permissionButton = document.getElementById('html5-qrcode-button-camera-permission');
+        if (permissionButton) {
+          permissionButton.click(); // Simula un clic en el botón existente
+        }
+        // const stopButton = document.getElementById('html5-qrcode-button-camera-start');
+        // if (stopButton) {
+        //   stopButton.click(); // Simula un clic en el botón existente
+        // }
+        // await verifyPermissions()
+        
+      };
+
+    // TODO: cuando se use el componente bien, al abrir el modal se tiene que abrir activar la acmara con el boton y al cerrar lo mismo, porque sino la camara va a estar siempre prendida
+
     return (
-        <div id={qrcodeRegionId} style={{width: '20rem'}}/>
+        <div className='html5-qrcode-container'>
+            <div id={qrcodeRegionId} style={{width: '20rem'}}/>
+            {showButton && (
+              <button className="button-camera-permission" type="button" onClick={handleClick}>
+                Click here to request camera permissions
+              </button>
+            )}
+        </div>
     );
 };
 
-export default Html5QrcodePlugin;
+export default QrScanner;
